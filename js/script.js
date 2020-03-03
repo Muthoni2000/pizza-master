02aaa94c322e3e9699ec87d9e2675c82e374b270
Muthoni2000 <bmuthoni39@yahoo.com>
2020-03-03 08:41:38 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,142 +1,90 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+// @@ -1,4 +1,3 @@
+
+
+//  * jQuery JavaScript Library v3.4.1
+//  * https://jquery.com/
+// @@ -189,7 +188,7 @@ jQuery.fn = jQuery.prototype = {
+// 	pushStack: function( elems ) {
+//     $('.carousel').carousel({
+//       interval: 2000
+//     })
+  
+    //business logic
+  function Order (type, size, crust, topping) {
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
   }
   
-  //display nav bar
-  $(document).ready(function(){
-  $(".about-nav").on("click", function(){
-    location.hash = "#about-us"
-  });
-  $(".menu-nav").on("click", function(){
-    location.hash = "#menu"
-  });
-  $(".order-online").on("click", function(){
-    
-    location.hash = "#order-online"
-  });
-  $("#take-out").on("click", function(){
-    $(".delivery").hide();
-  });
-  $("#delivery").on("click", function(){
-    $("#take-out").hide();
-  });
-  $("#delivery").on("click", function(){
-    $("#location").show();
-  })
-    $(".placeOrder").on("click", function(){
-      $(".displayTotal").show();
-  });
-  });
-  function invoice() {
-
-    // Setting size and sizePrice based on selection
-      var size = document.querySelector('input[name="size"]:checked').value; console.log("size="+size);
-      var sizePrice = 0;
-      if (size === "Regular") {
-        sizePrice = 600;
-      };
-      if (size === "Medium") {
-        sizePrice = 1000;
-      };
-      if (size === "Large") {
-        sizePrice = 1400;
-      }; console.log("sizePrice="+sizePrice);
-
-      // Setting crust and crustPrice based on selection
-  var crust = document.querySelector('input[name="crust"]:checked').value; console.log("crust="+crust);
-  var crustPrice = 0
-  if (crust="Cheese-Stuffed") {
-    crustPrice = 300;
-  }; console.log("crustPrice="+crustPrice);
-
-// Setting delivery based on selection
-  var delivery = document.querySelector                                   ('input[name="delivery"]:checked').value; console.log("delivery="+delivery);
-   var deliveryprice = 0
-   if (delivery = "Delivery"){
-     deliveryPrice = 200;
-      console.log("deliveryPrice="+deliveryPrice);
-   }
-   // Setting toppingsSelect array and toppingsPrice based on multiple check boxes selected. Also setting up toppingsDisplay to convert the array into a convenient string.
-  var toppingsPrice = 0
-  var toppings = document.getElementsByName("toppings");
-  var toppingsSelect = [];
-  var toppingsDisplay = "";
-  for (var i = 0; i < toppings.length; i++) {
-    if (toppings[i].checked) {
-      toppingsSelect.push(toppings[i].value);
-    };
-  }; console.log("toppingsSelect="+toppingsSelect);
-  if (toppingsSelect.length === 1) {
-    toppingsDisplay = toppingsSelect[0];
-  } else if (toppingsSelect.length > 1) {
-    for (i=0; i < toppingsSelect.length; i++) {
-        toppingsDisplay += toppingsSelect[i];
-        if (i < (toppingsSelect.length-1)) {
-          toppingsDisplay += ", ";
-        };
-    };
-  };  
-  toppingsPrice = toppingsSelect.length * 200; console.log("toppingsPrice="+toppingsPrice);
-  if (toppingsSelect.length === 0) {
-    toppingsDisplay = "No toppings";
-  }
-  // Setting vegSelect array and vegPrice based on multiple check boxes selected
-  var vegPrice = 0;
-  var veg = document.getElementsByName("veg");
-  var vegSelect = [];
-  var vegDisplay = "";
-  for (var i = 0; i<veg.length; i++) {
-    if (veg[i].checked) {
-      vegSelect.push(veg[i].value);
-    };
-  }; console.log("vegSelect="+vegSelect);
-  if (vegSelect.length > 4) {
-    vegPrice = 2;
-  }; console.log("vegPrice="+vegPrice);
-  if (vegSelect.length === 1) {
-    vegDisplay = vegSelect[0];
-  } else if (vegSelect.length > 1) {
-    for (i=0; i < vegSelect.length; i++) {
-      vegDisplay += vegSelect[i];
-      if (i < (vegSelect.length - 1)) {
-        vegDisplay += ", ";
-      };
-    };
+  Order.prototype.fullOrder = function () {
+    return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
   };
   
-  if (vegSelect.length === 0) {
-    vegDisplay = "No Vegetables";
+  function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
   }
-
-// Calculating totalPrice
-var totalPrice = (vegPrice + toppingsPrice + crustPrice + deliveryPrice  + sizePrice); console.log("totalPrice="+totalPrice);
-
-// Enter values into the invoice table
-$("#size").html(size +" Pizza");
-$("#sizeprice").html("shs"+sizePrice+".00");
-$("#crust").html(crust +" Crust");
-$("#crustprice").html("shs"+crustPrice+".00");
-$("#delivery").html(delivery +" Delivery");
-$("#deliveryprice").html("shs" +deliveryPrice+ ".00");
-$("#toppings").html(toppingsDisplay);
-$("#toppingsprice").html("shs"+toppingsPrice+".00");
-$("#veg").html(vegDisplay);
-$("#vegprice").html("shs"+vegPrice+".00");
-$("#totalprice").html(" Shs."+totalPrice+".00");
-};
-$(document).ready(function(){
-    $(".delivery").click(function(){
-    $("#takeout").hide();
-      $(".note").show();
-      $(".location").show();
+  
+  Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+  };
+  
+  
+  var sizePrice = [1000, 800, 600]
+  var deliverPrices = [0, 200];
+  //user interface logic
+  $(document).ready(function () {
+    $('form#myForm').submit(function (event) {
+        event.preventDefault();
+        var pizzaType = $('#type').val();
+  
+        var pizzaSize = parseInt($('#size').val());
+  
+        var pizzaCrust = $('#crust').val();
+  
+        var pizzaTop = $('#top').val();
+  
+        var pizzaQty = parseInt($('#qty').val());
+  
+        var pizzaPick = parseInt($('#pick').val());
+  
+  
+        var price = sizePrice[pizzaSize - 1];
+  
+  
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
+  
+  
+  
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your order is: " + newOrder.fullOrder() + ". Continue to see your total bill");
+        alert("Your bill is: " + newTotal.finalTotal());
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter the location of your delivery.");
+                alert("Your order has been received and it will be delivered. Continue to see  the details of your order");
+                alert("Your order is : " + newOrder.fullOrder() + ". Click okay to see your total bill");
+                alert("Your bill is : " + newTotal.finalTotal());
+            }
+        }
+  
     });
-    $("#calc").click(function(){
-      $(".displayTotal").show();
-      $(".pizzaSelector").hide();
-    })
+  
+  
+  
+  
+    //sending message function
+    $('form#contactForm').submit(function (event) {
+        event.preventDefault();
+        var name = $('#name').val();
+        var pass = $('#email').val();
+        var mess = $('#mess').val();
+        alert("Hi " + name + ",  We have received your message and We will  be getting in touch with you shortly. Thank you for contacting us.");
+    });
+  
+  
   });
